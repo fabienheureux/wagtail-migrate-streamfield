33f54cb19d61e79ff4b4a966b223e02b3e208367
Fabien Le Frapper <contact@fabienlefrapper.me>
2023-03-10 21:25:11 +0100

    Initial commit

---
+++ example/home/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.blocks import StreamBlock, StructBlock
+from wagtail.fields import StreamField
+from wagtail.admin.panels import FieldPanel
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class ExtendedImageBlock(StructBlock):
+    image = ImageChooserBlock()
+
+
+class StoryBlock(StreamBlock):
+    image = ImageChooserBlock()
+
+
+class HomePage(Page):
+    body = StreamField(StoryBlock(), use_json_field=True)
+
+    content_panels = [FieldPanel("body")]
